7753a282a62a316fed1049404c542cf4e64ef82c
kamrul25 <kamrul.coder01@gmail.com>
2023-05-25 23:38:42 +0600

    complete footer

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-6xl text-red-600">Hi, I am from home page</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,34 @@
+import { FaInstagram, FaFacebookF, FaTwitter } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer className="w-full p-10  rounded">
+      <div className="w-full flex">
+        <div className="w-1/2 footer-center py-20  bg-[#1F2937] text-white">
+          <h1>CONTACT US</h1>
+          <div className="space-y-2 mt-4">
+            <p> 123 ABS Street, Uni 21, Bangladesh </p>
+            <p>+88 123456789</p>
+            <p>Mon - Fri: 08:00 - 22:00</p>
+            <p>Sat - Sun: 10:00 - 23:00</p>
+          </div>
+        </div>
+        <div className=" w-1/2 bg-neutral text-white footer-center py-20 space-y-4">
+          <h1 className="footer-title">Follow Us</h1>
+          <p>Join us on Social Media</p>
+          <div className="flex justify-center gap-4 text-3xl text-white ">
+            <FaFacebookF />
+            <FaInstagram />
+            <FaTwitter />
+          </div>
+        </div>
+      </div>
+
+      <div className=" footer-center text-base-300 bg-base-content py-4">
+        <p>Copyright © CulinaryCloud. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
