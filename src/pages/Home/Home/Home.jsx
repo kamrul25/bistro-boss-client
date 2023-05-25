25e08d9dd1b9a3fa1e5e7beaa48553ee53d457f5
kamrul25 <kamrul.coder01@gmail.com>
2023-05-26 01:53:20 +0600

    added some section in home page

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,8 +1,18 @@
+import Banner from "../Banner/Banner";
+import Category from "../Category/Category";
+import Featured from "../Featured/Featured";
+import PopularMenu from "../PopularMenu/PopularMenu";
+import Testimonials from "../Testimonials/Testimonials";
+
 
 const Home = () => {
     return (
         <div>
-            <h1 className="text-6xl text-red-600">Hi, I am from home page</h1>
+           <Banner></Banner>
+           <Category></Category>
+           <PopularMenu></PopularMenu>
+           <Featured></Featured>
+           <Testimonials></Testimonials>
         </div>
     );
 };
--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -2,9 +2,9 @@ import { FaInstagram, FaFacebookF, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="w-full p-10  rounded">
-      <div className="w-full flex">
-        <div className="w-1/2 footer-center py-20  bg-[#1F2937] text-white">
+    <footer className="w-full  rounded">
+      <div className="w-full flex ">
+        <div className="w-1/2 footer-center py-20  bg-[#1F2937]  text-white">
           <h1>CONTACT US</h1>
           <div className="space-y-2 mt-4">
             <p> 123 ABS Street, Uni 21, Bangladesh </p>
@@ -13,7 +13,7 @@ const Footer = () => {
             <p>Sat - Sun: 10:00 - 23:00</p>
           </div>
         </div>
-        <div className=" w-1/2 bg-neutral text-white footer-center py-20 space-y-4">
+        <div className=" w-1/2  bg-[#111827] text-white footer-center py-20 space-y-4">
           <h1 className="footer-title">Follow Us</h1>
           <p>Join us on Social Media</p>
           <div className="flex justify-center gap-4 text-3xl text-white ">
@@ -24,7 +24,7 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className=" footer-center text-base-300 bg-base-content py-4">
+      <div className=" footer-center text-base-300 bg-[#151515] py-4">
         <p>Copyright © CulinaryCloud. All rights reserved.</p>
       </div>
     </footer>
